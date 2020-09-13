a5ad966ea112c7c4c03890c00cf9502b7d510e61
Viraj Savaliya <65527752+virsav@users.noreply.github.com>
2020-09-12 23:17:00 -0700

    java version 3

--- app/src/main/java/edu/asu/ame/meteor/speedytiltshift2018/SpeedyTiltShift.java
+++ app/src/main/java/edu/asu/ame/meteor/speedytiltshift2018/SpeedyTiltShift.java
@@ -23,161 +23,77 @@ public class SpeedyTiltShift {
             return arr[y*width+x];
 
     }
+    public  static float Gk(int k,float sigma){
+        return (float) ((Math.exp(-((k * k) / (2 * sigma * sigma)))) * (1 / Math.sqrt(2 * 3.14 * sigma * sigma)));
+    }
 
+    public static int finalpixelval(int x,int y,Bitmap b,int[] arr,float sigma,int flag){
+        int R=0, G=0, B=0, R1=0, G1=0, B1=0,val=0;
+        float vectorG_norm=0;
+        int r = (int)Math.ceil(2*sigma);
+        for (int k = -1*r; k <= r; k++) {
+            if(flag==0){
+                val=getFinalpixelval(x+k, y, b);
+            }
+            else if(flag==1){
+                val=getFinalpixelvalarr(x, y+k, b.getHeight(),b.getWidth(),arr);
+            }
+            B = val & 0xff;
+            G = (val >> 8) & 0xff;
+            R = (val >> 16) & 0xff;
+            vectorG_norm = Gk(k,sigma);
+            B1 += vectorG_norm * B;
+            G1 += vectorG_norm * G;
+            R1 += vectorG_norm * R;
+        }
+        int A = 0xff;
+        int color =((A & 0xff) << 24) | ((R1 & 0xff) << 16) | ((G1 & 0xff) << 8 )| (B1 & 0xff);
+        return color;
+
+    }
+    public static float sigmacal(int y, float sigma_far, float sigma_near, int a0, int a1, int a2, int a3){
+        float sigma_one=0;
+        if(y<a0){
+            sigma_one = sigma_far;
+        }
+        else if(y>=a0 && y<a1){
+            sigma_one = sigma_far*((float)(a1-y)/(float)(a1-a0));
+        }
+        else if(y>a2 && y<=a3){
+            sigma_one = sigma_near*((float)(y-a2)/(float)(a3-a2));
+        }
+        else if(y>a3){
+            sigma_one = sigma_near;
+        }
+        return sigma_one;
+    }
     public static Bitmap tiltshift_java(Bitmap input, float sigma_far, float sigma_near, int a0, int a1, int a2, int a3){
         Bitmap outBmp = Bitmap.createBitmap(input.getWidth(), input.getHeight(), Bitmap.Config.ARGB_8888);
         //cannot write to input Bitmap, since it may be immutable
         //if you try, you may get a java.lang.IllegalStateException
-
-        int[] pixels = new int[input.getHeight()*input.getWidth()];
         int[] pixelsinn = new int[input.getHeight()*input.getWidth()];
         int[] pixelsOut = new int[input.getHeight()*input.getWidth()];
-        input.getPixels(pixels,0,input.getWidth(),0,0,input.getWidth(),input.getHeight());
 
-        int R=0, G=0, B=0, R1=0, G1=0, B1=0;
-
-        int r_far = (int)Math.ceil(2*sigma_far);
-        int r_near = (int)Math.ceil(2*sigma_near);
 
         float sigma_one=0;
-
-        double vectorG_norm=0;
-
         for(int y = 0; y < input.getHeight(); y++) {
             for (int x=0;x<input.getWidth();x++) {
-
-                if(y<a0 && sigma_far>0.6){
-                    sigma_one = sigma_far;
-                    for (int k = -1*r_far; k <= r_far; k++) {
-                        int val=getFinalpixelval(x+k, y, input);
-                        B = val & 0xff;
-                        G = (val >> 8) & 0xff;
-                        R = (val >> 16) & 0xff;
-                        vectorG_norm =  ((Math.exp(-((k * k) / (2 * sigma_one * sigma_one)))) * (1 / Math.sqrt(2 * 3.14 * sigma_one * sigma_one)));
-                        B1 += vectorG_norm * B;
-                        G1 += vectorG_norm * G;
-                        R1 += vectorG_norm * R;
-                    }
+                if(!(y>=a1 && y<=a2)){
+                    sigma_one= sigmacal(y,sigma_far,sigma_near,a0,a1,a2,a3);
+                    pixelsinn[y*input.getWidth()+x]= finalpixelval(x,y,input,pixelsinn,sigma_one,0);
                 }
-                else if(y>=a0 && y<a1 && sigma_far>0.6){
-                    sigma_one = sigma_far*((float)(a1-y)/(float)(a1-a0));
-                    for (int k = -1*r_far; k <= r_far; k++) {
-                        int val=getFinalpixelval(x+k, y, input);
-                        B = val & 0xff;
-                        G = (val >> 8) & 0xff;
-                        R = (val >> 16) & 0xff;
-                        vectorG_norm =  ((Math.exp(-((k * k) / (2 * sigma_one * sigma_one)))) * (1 / Math.sqrt(2 * 3.14 * sigma_one * sigma_one)));
-                        B1 += vectorG_norm * B;
-                        G1 += vectorG_norm * G;
-                        R1 += vectorG_norm * R;
-                    }
-                }
-                else if(y>a2 && y<=a3 && sigma_near>0.6){
-                    sigma_one = sigma_near*((float)(y-a2)/(float)(a3-a2));
-                    for (int k = -1*r_near; k <= r_near; k++) {
-                        int val=getFinalpixelval(x+k, y, input);
-                        B = val & 0xff;
-                        G = (val >> 8) & 0xff;
-                        R = (val >> 16) & 0xff;
-                        vectorG_norm =  ((Math.exp(-((k * k) / (2 * sigma_one * sigma_one)))) * (1 / Math.sqrt(2 * 3.14 * sigma_one * sigma_one)));
-                        B1 += vectorG_norm * B;
-                        G1 += vectorG_norm * G;
-                        R1 += vectorG_norm * R;
-                    }
-                }
-                else if(y>a3 && sigma_near>0.6){
-                    sigma_one = sigma_near;
-                    for (int k = -1*r_near; k <= r_near; k++) {
-                        int val=getFinalpixelval(x+k, y, input);
-                        B = val & 0xff;
-                        G = (val >> 8) & 0xff;
-                        R = (val >> 16) & 0xff;
-                        vectorG_norm =  ((Math.exp(-((k * k) / (2 * sigma_one * sigma_one)))) * (1 / Math.sqrt(2 * 3.14 * sigma_one * sigma_one)));
-                        B1 += vectorG_norm * B;
-                        G1 += vectorG_norm * G;
-                        R1 += vectorG_norm * R;
-                    }
-                }
-                else{
-                    int val=getFinalpixelval(x, y, input);
-                    B1 = val & 0xff;
-                    G1 = (val >> 8) & 0xff;
-                    R1 = (val >> 16) & 0xff;
-                }
-
-                int A = 0xff;
-                int color =((A & 0xff) << 24) | ((R1 & 0xff) << 16) | ((G1 & 0xff) << 8 )| (B1 & 0xff);
-                B1=0;G1=0;R1=0;
-                pixelsinn[y*input.getWidth()+x]=color;
+                else
+                    pixelsinn[y*input.getWidth()+x]=input.getPixel(x,y);
             }
         }
-
-        B1=0;G1=0;R1=0;
         for(int y = 0; y < input.getHeight(); y++) {
             for (int x=0;x<input.getWidth();x++) {
-                if(y<a0 && sigma_far>0.6){
-                    sigma_one = sigma_far;
-                    for (int k = -1*r_far; k <= r_far; k++) {
-                        int val= getFinalpixelvalarr(x,y+k,input.getHeight(),input.getWidth(),pixelsinn);
-                        B =  val & 0xff;
-                        G = (val >> 8) & 0xff;
-                        R = (val >> 16) & 0xff;
-                        vectorG_norm =  ((Math.exp(-((k * k) / (2 * sigma_one * sigma_one)))) * (1 / Math.sqrt(2 * 3.14 * sigma_one * sigma_one)));
-                        B1 += vectorG_norm * B;
-                        G1 += vectorG_norm * G;
-                        R1 += vectorG_norm * R;
-                    }
+                if(!(y>=a1 && y<=a2)){
+                    sigma_one= sigmacal(y,sigma_far,sigma_near,a0,a1,a2,a3);
+                    pixelsOut[y*input.getWidth()+x]= finalpixelval(x,y,input,pixelsinn,sigma_one,1);
                 }
-                else if(y>=a0 && y<a1 && sigma_far>0.6){
-                    sigma_one = sigma_far*((float)(a1-y)/(float)(a1-a0));
-                    for (int k = -1*r_far; k <= r_far; k++) {
-                        int val= getFinalpixelvalarr(x,y+k,input.getHeight(),input.getWidth(),pixelsinn);
-                        B =  val & 0xff;
-                        G = (val >> 8) & 0xff;
-                        R = (val >> 16) & 0xff;
-                        vectorG_norm =  ((Math.exp(-((k * k) / (2 * sigma_one * sigma_one)))) * (1 / Math.sqrt(2 * 3.14 * sigma_one * sigma_one)));
-                        B1 += vectorG_norm * B;
-                        G1 += vectorG_norm * G;
-                        R1 += vectorG_norm * R;
-                    }
-                }
-                else if(y>a2 && y<=a3 && sigma_near>0.6){
-                    sigma_one = sigma_near*((float)(y-a2)/(float)(a3-a2));
-                    for (int k = -1*r_near; k <= r_near; k++) {
-                        int val= getFinalpixelvalarr(x,y+k,input.getHeight(),input.getWidth(),pixelsinn);
-                        B =  val & 0xff;
-                        G = (val >> 8) & 0xff;
-                        R = (val >> 16) & 0xff;
-                        vectorG_norm =  ((Math.exp(-((k * k) / (2 * sigma_one * sigma_one)))) * (1 / Math.sqrt(2 * 3.14 * sigma_one * sigma_one)));
-                        B1 += vectorG_norm * B;
-                        G1 += vectorG_norm * G;
-                        R1 += vectorG_norm * R;
-                    }
-                }
-                else if(y>a3 && sigma_near>0.6){
-                    sigma_one = sigma_near;
-                    for (int k = -1*r_near; k <= r_near; k++) {
-                        int val= getFinalpixelvalarr(x,y+k,input.getHeight(),input.getWidth(),pixelsinn);
-                        B =  val & 0xff;
-                        G = (val >> 8) & 0xff;
-                        R = (val >> 16) & 0xff;
-                        vectorG_norm =  ((Math.exp(-((k * k) / (2 * sigma_one * sigma_one)))) * (1 / Math.sqrt(2 * 3.14 * sigma_one * sigma_one)));
-                        B1 += vectorG_norm * B;
-                        G1 += vectorG_norm * G;
-                        R1 += vectorG_norm * R;
-                    }
-                }
-                else{
-                    int val= getFinalpixelvalarr(x,y,input.getHeight(),input.getWidth(),pixelsinn);
-                    B1 =  val & 0xff;
-                    G1 = (val >> 8) & 0xff;
-                    R1 = (val >> 16) & 0xff;
-                }
-
-                int A = 0xff;
-                int color =((A & 0xff) << 24) | ((R1 & 0xff) << 16) | ((G1 & 0xff) << 8 )| (B1 & 0xff);
-                B1=0;G1=0;R1=0;
-                pixelsOut[y*input.getWidth()+x]=color;
+                else
+                    pixelsOut[y*input.getWidth()+x]=input.getPixel(x,y);
             }
         }
 
@@ -185,6 +101,7 @@ public class SpeedyTiltShift {
 
         return outBmp;
     }
+
     public static Bitmap tiltshift_cpp(Bitmap input, float sigma_far, float sigma_near, int a0, int a1, int a2, int a3){
         Bitmap outBmp = Bitmap.createBitmap(input.getWidth(), input.getHeight(), Bitmap.Config.ARGB_8888);
         int[] pixels = new int[input.getHeight()*input.getWidth()];
